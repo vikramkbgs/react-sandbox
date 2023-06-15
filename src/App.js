98d41d41ec82564e3b411bd27143cbd99c808c02
vikramkbgs@gmail.com <vikramkbgs@gmail.com>
2023-06-15 19:03:56 +0530

    completed the SocialIcon

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
+import "./App.css";
+import Social from "./Social";
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  const socialComponents = Array.from({ length: 4 }, (_, index) => (
+    <Social key={index} />
+  ));
+
+  return <div className="App">{socialComponents}</div>;
 }
 
 export default App;
